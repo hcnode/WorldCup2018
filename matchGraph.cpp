aa3e8d43df4fade611929681518700c19b7c68f2
masghari78 <mhosseinasghari025@gmail.com>
2018-01-16 01:21:44 +0330

    add graph of matches

---
+++ matchGraph.cpp
@@ -0,0 +1,345 @@
+#include <stdio.h>
+int main()
+{
+	char * teamA1="    Argentina     ";
+	char *teamE1= "    Australia     ";
+	/////matches graph?
+	///////////////two teams on one line
+	int numcolumns=8;
+	int counter=0;
+	int game=1;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("--------------------");
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("|%s|",teamA1);
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("--------------------");
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+	////////////////////////////////////////////////////
+	if(game==0)
+	{
+		for(int i=0;i<27;i++)
+		{
+			puts("");
+		}
+		numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("--------------------");
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("|%s|",teamA1);
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("--------------------");
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+		
+	}
+	else if(game==1)
+	{
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("--------------------");
+			printf("                            ");
+			
+		}
+		puts("");
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("|%s|",teamA1);
+			printf("                            ");
+			
+		}
+		puts("");
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("--------------------");
+			printf("                            ");
+			
+		}
+		////////////
+		for(int i=0;i<15;i++)
+		{
+			puts("");
+		}
+		/////////////
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("--------------------");
+			printf("                            ");
+			
+		}
+		puts("");
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("|%s|",teamA1);
+			printf("                            ");
+			
+		}
+		puts("");
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("--------------------");
+			printf("                            ");
+			
+		}
+		puts("");
+		///////
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("--------------------");
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("|%s|",teamA1);
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("--------------------");
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+		
+	}
+	else if(game==2)
+	{
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("--------------------");
+			printf("                            ");
+			
+		}
+		puts("");
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("|%s|",teamA1);
+			printf("                            ");
+			
+		}
+		puts("");
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("--------------------");
+			printf("                            ");
+			
+		}
+		////////////
+		puts("");
+		printf("                     ");	
+		printf("--------------------");
+		printf("                                                                            ");
+		printf("--------------------");
+		puts("");
+		printf("                     ");
+		printf("                            ");
+		for(int i=0;i<2;i++)
+		{
+			
+			printf("|%s|",teamA1);
+			printf("                                                                            ");
+			
+		}
+		puts("");
+		printf("                     ");
+		for(int i=0;i<2;i++)
+		{
+			
+			printf("--------------------");
+			printf("                                                                            ");
+			
+		}
+		////////////
+		for(int i=0;i<9;i++)
+		{
+			puts("");
+		}
+		/////////////
+		printf("                        ");
+		for(int i=0;i<2;i++)
+		{
+			
+			printf("--------------------");
+			printf("                                                        ");
+			
+		}
+		puts("");
+		printf("                        ");
+		for(int i=0;i<2;i++)
+		{
+			
+			printf("|%s|",teamA1);
+			printf("                                                        ");
+			
+		}
+		puts("");
+		printf("                        ");
+		for(int i=0;i<2;i++)
+		{
+			
+			printf("--------------------");
+			printf("                                                        ");
+			
+		}
+		////////////
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("--------------------");
+			printf("                            ");
+			
+		}
+		puts("");
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("|%s|",teamA1);
+			printf("                            ");
+			
+		}
+		puts("");
+		printf("            ");
+		for(int i=0;i<4;i++)
+		{
+			
+			printf("--------------------");
+			printf("                            ");
+			
+		}
+		puts("");
+		///////
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("--------------------");
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("|%s|",teamA1);
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+	numcolumns=8;
+	while (numcolumns)
+	{
+		if(counter==0)
+		{
+		printf("--------------------");
+		printf("    ");
+		}
+		numcolumns--;
+		
+	}
+	puts("");
+		
+	}
+	
+	
+	
+}
