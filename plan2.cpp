fac69ac2753931c21d644a5a3ac27a5b5e94c223
horr <xhamid0011@gmail.com>
2018-01-16 22:29:29 +0330

    new table is allready uploaded

---
+++ plan2.cpp
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<string.h>
+void table2(int n){
+	
+		int i=0,j=0;
+	int counter=0;
+	int x=0,y=0;
+	char teamTable[32][15]={"  Argentina  ","  Australia  ","   Belgium   ","    Brazil   ","   Colombia  ","  Costa Rica ","   Croatia   ","   Denmark   ","    Egypt    ","   England   ","    France   ","   Germany   ","   Iceland   ","     Iran    ","    Japan    ","    Korea    ","    Mexico   ","   Morocco   ","   Nigeria   ","    Panama   ","     Peru    ","    Poland   ","   Portugal  ","    Russia   "," Saudi Arabia ","   Senegal   ","    Serbia   ","    Spain    ","    Sweden   "," Switzerland ","   Tunisia   ","   Uruguay   "};
+	
+	char teamBoard[45][5][15];
+	for(i = 0;i<45;i++){
+		for(j=0;j<5;j++){
+			strcpy(teamBoard[i][j],"0");
+		}
+	}
+	for(j=0;j<n;j++)
+	{
+		if(j=0)
+		{
+			for(i=0;i<45;i+=2)
+			{
+				strcpy(teamBoard[i][j],teamTable[(p->number)-1]);					
+			}
+		}
+		if(j=1)
+		{
+			for(i=1;i<45;i+=4)
+			{
+				strcpy(teamBoard[i][j],teamTable[(p->number)-1]);					
+			}
+		}
+		if(j=3)
+		{
+			for(i=4;i<45;i+=7)
+			{
+				strcpy(teamBoard[i][j],teamTable[(p->number)-1]);					
+			}
+		}
+		if(j=4)
+		{
+			for(i=4;i<45;i+=15)
+			{
+				strcpy(teamBoard[i][j],teamTable[(p->number)-1]);					
+			}
+		}
+	}
+	for(i =0;i<45;i++)
+	{
+		printf("\n");
+		for(j=0;j<5;j++)
+		{
+			
+			if(strcmp(teamBoard[i][j],"0")!=0)
+			{
+			printf("|");
+			printf("%s",teamBoard[i][j]);
+			printf("|");
+			}
+			if(strcmp(teamBoard[i][j],"0")==0)
+			{
+			printf("               ");
+			}
+		}
+		printf("\n");	
+	}	
+}
+
+
+int main(){
+	table2();
+}
